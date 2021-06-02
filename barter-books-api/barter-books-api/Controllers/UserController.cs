23f390841d8b36ee9f695a44df790023f03ce89a
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-02 17:17:24 -0500

    completed user controller to handle crud requests.

--- barter-books-api/barter-books-api/Controllers/UserController.cs
+++ barter-books-api/barter-books-api/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using barter_books_api.DataAccess;
+using barter_books_api.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,9 +9,57 @@ using System.Threading.Tasks;
 
 namespace barter_books_api.Controllers
 {
-    [Route("api/User")]
+    [Route("api/Users")]
     [ApiController]
     public class UserController : FirebaseEnabledController
     {
+        UserRepository _repo;
+
+        public UserController()
+        {
+            _repo = new UserRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleUserById(string id)
+        {
+            var user = _repo.GetSingleUser(id);
+
+            if (user == null)
+            {
+                return NotFound("This user does not exist.");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddAUser(User user)
+        {
+            _repo.AddAUser(user);
+            return Created($"api/Users/{user.Id}", user);
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateUser(User user)
+        {
+            _repo.UpdateUser(user);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(string id)
+        {
+            _repo.DeleteUser(id);
+
+            return Ok();
+        }
     }
 }
