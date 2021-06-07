2bb1d9fd48f4ad4133932f229b9d9ccbd089d142
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-07 15:38:16 -0500

    Collection model.

---
+++ barter-books-api/barter-books-api/Models/Collection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace barter_books_api.Models
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+    }
+}
