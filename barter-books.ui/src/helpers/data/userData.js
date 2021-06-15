d3ed46ce551c6461ace47c8e91bebaf205ffd92f
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-15 12:02:38 -0500

    created userData file and added config.json to gitignore.

---
+++ barter-books.ui/src/helpers/data/userData.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { baseUrl } from './config.json';
+
+const userDataUrl = `${baseUrl}/Users`;
+
+const getSingleUser = (id) => new Promise((resolve, reject) => {
+  axios.get(`${userDataUrl}/${id}`).then((response) => {
+    resolve(response.data);
+  }).catch((error) => reject(error));
+});
+
+export default { getSingleUser };
