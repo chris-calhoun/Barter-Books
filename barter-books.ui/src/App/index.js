c98ac5afd2de11f90848b976da3f0d8c54d6f39f
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-15 15:01:33 -0500

    used firebase to check if there is a user and set state accordingly. Passed to Router components.

--- barter-books.ui/src/App/index.js
+++ barter-books.ui/src/App/index.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import { BrowserRouter as Router } from 'react-router-dom';
+import fbConnection from '../helpers/data/fbConnection';
 import MyNavbar from '../components/MyNavbar';
 import Routes from '../helpers/Routes';
 import '../styles/App.scss';
 
-function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <MyNavbar />
-        <Routes />
-      </Router>
-    </div>
-  );
+fbConnection();
+
+class App extends React.Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        user
+          .getIdToken()
+          .then((token) => sessionStorage.setItem('token', token));
+        this.setState({ user });
+      } else {
+        this.setState({ user: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Router>
+          <MyNavbar user={this.state.user} />
+          <Routes user={this.state.user} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
