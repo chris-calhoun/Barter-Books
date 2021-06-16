87bec804cd5b863c79d6cae83a9ffa7c849ca829
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-15 20:02:18 -0500

    got book titles to show up, just not in the right place. Make it look good, and then will be done with ticket.

--- barter-books.ui/src/components/SearchBar/index.js
+++ barter-books.ui/src/components/SearchBar/index.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react';
 import {
   Form,
-  FormGroup,
   Input,
 } from 'reactstrap';
+import BookData from '../../helpers/data/bookData';
 
 export default class index extends Component {
+  state = {
+    books: [],
+    searchText: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    BookData.getSearchedBooks(this.state.searchText).then((response) => {
+      this.setState({
+        books: response,
+        searchText: '',
+      });
+    });
+  }
+
   render() {
+    const { books, searchText } = this.state;
+
+    const renderBooks = () => (
+      books.map((book) => <>{book.volumeInfo.title}
+      </>)
+    );
+
     return (
-      <Form>
-        <FormGroup>
-          <Input
-            type='search'
-            name='bookSearch'
-            id='bookSearch'
-            placeholder='enter a book title or author'
-          />
-        </FormGroup>
-      </Form>
+      <>
+        <Form onSubmit={this.handleSubmit}>
+            <Input
+              type='search'
+              name='searchText'
+              id='searchText'
+              placeholder='enter a book title or author'
+              value={searchText}
+              onChange={this.handleChange}
+            />
+        </Form>
+        {books !== [] && <div className='d-flex flex-wrap container'>{renderBooks()}</div>}
+      </>
     );
   }
 }
