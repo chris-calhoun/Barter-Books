83023f199ce29c11c0b8fc504ecebad138330209
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-16 07:43:52 -0500

    passed books from search bar to search results view.

--- barter-books.ui/src/views/SearchResultsView.js
+++ barter-books.ui/src/views/SearchResultsView.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import BookData from '../helpers/data/bookData';
 
-export default function SearchResultsView() {
-  return (
-    <div>
-      <h1>Search Results View</h1>
-    </div>
-  );
+export default class SearchResultsView extends Component {
+  state = {
+    searchText: '',
+    books: [],
+  };
+
+  componentDidMount() {
+    this.showResults();
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.match.params.term !== this.props.match.params.term) {
+      this.showResults();
+    }
+  }
+
+  showResults = () => {
+    const searchText = this.props.match.params.term;
+
+    BookData.getSearchedBooks(searchText).then((response) => {
+      this.setState({
+        books: response,
+        searchText,
+      });
+    });
+  }
+
+  render() {
+    const { books } = this.state;
+
+    const renderBooks = () => (
+      books.map((book) => <p key={book.id}>{book.volumeInfo.title}</p>)
+    );
+    return (
+      <div>
+        <h1>Search Results</h1>
+        {books !== [] && <div className='d-flex flex-wrap container'>{renderBooks()}</div>}
+      </div>
+    );
+  }
 }
