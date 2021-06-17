cf197dd1cb26e10ef91524a8104557fcb9807db4
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-17 14:27:04 -0500

    completed initial styling of login page.

--- barter-books.ui/src/views/LoginView.js
+++ barter-books.ui/src/views/LoginView.js
@@ -3,9 +3,14 @@ import Auth from '../components/Auth';
 
 export default function LoginView() {
   return (
-    <div>
-      <h2>Login.</h2>
-      <Auth />
+    <div className='login-view'>
+      <div className='image-box'>
+        <h1 className='centered'>Barter Books</h1>
+        <img src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym9va3NoZWxmfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="books on a shelf"/>
+      </div>
+      <div className='content-box'>
+        <Auth />
+      </div>
     </div>
   );
 }
