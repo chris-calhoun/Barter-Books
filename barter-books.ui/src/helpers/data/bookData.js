76f168e78ed2efe8e7254cc7dd5cf80a851ddf43
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-17 11:56:27 -0500

    completed search results css.

--- barter-books.ui/src/helpers/data/bookData.js
+++ barter-books.ui/src/helpers/data/bookData.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { appApiKey } from './config.json';
+// import { appApiKey } from './config.json';
 
 const getSearchedBooks = (userSearch) => new Promise((resolve, reject) => {
   // remove apiApiKey if quota is hit during testing
-  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userSearch}&maxResults=25`)
   // axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userSearch}&maxResults=5&key=${appApiKey}`)
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userSearch}&maxResults=25`)
     .then((response) => {
       resolve(Object.values(response.data.items));
     }).catch((error) => reject(error));
