1fc4d62624b8935f6c55c24d312af0bd09a85881
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-17 16:44:27 -0500

    changed login view image.

--- barter-books.ui/src/views/LoginView.js
+++ barter-books.ui/src/views/LoginView.js
@@ -6,7 +6,7 @@ export default function LoginView() {
     <div className='login-view'>
       <div className='image-box'>
         <h1 className='centered'>Barter Books</h1>
-        <img src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym9va3NoZWxmfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="books on a shelf"/>
+        <img src="https://wallpaperaccess.com/full/2964938.jpg" alt="books on a shelf"/>
       </div>
       <div className='content-box'>
         <Auth />
