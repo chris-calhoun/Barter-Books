a60f06e08d45f026afe3fddb024a765759a21741
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-18 04:30:59 -0500

    created env file.

--- barter-books.ui/src/helpers/data/bookData.js
+++ barter-books.ui/src/helpers/data/bookData.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { appApiKey } from './config.json';
+import appApiKey from '../apiKey';
 
 const getSearchedBooks = (userSearch) => new Promise((resolve, reject) => {
   // remove apiApiKey if quota is hit during testing
   // axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userSearch}&maxResults=10`)
-  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userSearch}&maxResults=2&key=${appApiKey}`)
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${userSearch}&maxResults=2&key=${appApiKey.exApiKey}`)
     .then((response) => {
       resolve(Object.values(response.data.items));
     }).catch((error) => reject(error));
