69d1a579df6cac57fb547c993b85c0c22d24fece
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-19 11:53:15 -0500

    recently added books appear on timeline.

--- barter-books.ui/src/components/Cards/Books/bookRecentlyAddedCard.js
+++ barter-books.ui/src/components/Cards/Books/bookRecentlyAddedCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function BookRecentlyAddedCard() {
+export default function BookRecentlyAddedCard({ bookData }) {
   return (
     <div>
-      <h2>Recently added books will go here.</h2>
+        <div className='card-img-container'>
+          <img width="100%" src={bookData.image} alt={bookData.name} />
+        </div>
+      <p>Title: {bookData.name}</p>
+      <p>Author: {bookData.name}</p>
+      <p>Added by: {bookData.firstName} {bookData.lastName}</p>
+      <p>Data added: {bookData.dateAddedToCollection}</p>
     </div>
   );
 }
