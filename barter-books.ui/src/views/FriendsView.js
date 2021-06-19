1b476f7ec6b5ca774b5fbc01ca32a9de4b21b975
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-19 01:09:21 -0500

    component set up to change component on button click.

--- barter-books.ui/src/views/FriendsView.js
+++ barter-books.ui/src/views/FriendsView.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
 } from 'reactstrap';
 
-export default function FriendsView() {
+export default function FriendsView(props) {
+  const [view, setView] = useState('find-friends-btn');
+
+  let component = '';
+  switch (view) {
+    case 'find-friends-btn':
+      component = (
+      <>
+        <h2>find friends</h2>
+      </>);
+      break;
+    case 'followers-btn':
+      component = (
+      <>
+        <h2>followers</h2>
+      </>);
+      break;
+    case 'following-btn':
+      component = (
+      <>
+        <h2>find friends</h2>
+      </>);
+      break;
+    default:
+      console.warn('view not found.');
+  }
+
   return (
     <div className="friends-view">
       <div className="friend-buttons">
         <div className="find-friends">
-          <Button outline color="primary">find friends</Button>
+          <Button id="find-friends-btn" onClick={(e) => setView(e.target.id)} outline color="primary">find friends</Button>
         </div>
         <div className="followers">
-          <Button outline color="primary">followers</Button>
+          <Button id="followers-btn" onClick={(e) => setView(e.target.id)} outline color="primary">followers</Button>
         </div>
         <div className="following">
-          <Button outline color="primary">following</Button>
+          <Button id="following-btn" onClick={(e) => setView(e.target.id)} outline color="primary">following</Button>
         </div>
       </div>
       <div className="user-cards">
         <h2>user cards will go here</h2>
+        {component}
       </div>
     </div>
   );
