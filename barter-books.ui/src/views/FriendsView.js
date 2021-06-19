5ba374728e2d4128d29d6286603d354a70c090d8
Chris Calhoun <ctcalhoun2@gmail.com>
2021-06-19 00:07:38 -0500

    created buttons for friends view.

--- barter-books.ui/src/views/FriendsView.js
+++ barter-books.ui/src/views/FriendsView.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {
+  Button,
+} from 'reactstrap';
 
 export default function FriendsView() {
   return (
-    <div>
-      <h2>Friends</h2>
+    <div className="friends-view">
+      <div className="friend-buttons">
+        <div className="find-friends">
+          <Button outline color="primary">find friends</Button>
+        </div>
+        <div className="followers">
+          <Button outline color="primary">followers</Button>
+        </div>
+        <div className="following">
+          <Button outline color="primary">following</Button>
+        </div>
+      </div>
+      <div className="user-cards">
+        <h2>user cards will go here</h2>
+      </div>
     </div>
   );
 }
